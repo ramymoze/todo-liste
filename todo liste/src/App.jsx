75cf12e0dd4bbc50bff12328dfc7ba65b6b0aeca
ramy <ze.mohramy@gmail.com>
2025-02-04 00:23:57 +0100

    modified the title section

--- todo liste/src/App.jsx
+++ todo liste/src/App.jsx
@@ -11,7 +11,10 @@ function App() {
    <div className='flex'>
     <Navebar/>
     <div className='flex flex-col w-full'>
+    <div className='flex justify-between'>
+    <h1 className='text-5xl text-black m-6 p-6'>Today's  Liste </h1>
     <Newtask/>
+    </div>
     <div className='flex  justify-center items-center h-full'>
     <Task  taskname="test"/>
     </div>
--- todo liste/src/components/Newtask.jsx
+++ todo liste/src/components/Newtask.jsx
@@ -1,9 +1,15 @@
 function Newtask() {
+    
+    const add = () => {
+        
+    }
+    
     return (
+    
         <>
-           <div className="flex justify-between h-20 text-5xl text-black m-6 "> <h1>Today's  Liste </h1>
+           <div className="flex justify-end h-20 w-52 text-5xl text-black m-6 "> 
            <div>
-            <button className="text-2xl text-black border-[3px]  border-blue-600 rounded-lg m-2 p-2 bg-blue-200">
+            <button onClick={add} className="text-2xl text-black border-[3px]  border-blue-600 rounded-lg m-2 p-2 bg-blue-200">
                 + Newtask</button>
         </div></div>
 
